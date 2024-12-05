bb49f64dc76754c97fe735632f377609c43cc0f1
dvachette <donatien.vachette@hotmail.com>
2024-12-05 20:21:37 +0100

    fix de l'arborescence

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
+print(os.getcwd())
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['template_folder'] = 'threejs'
 @app.route('/')
 def index():
-    return 'Hello World !'
+    return render_template("threejs/index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
