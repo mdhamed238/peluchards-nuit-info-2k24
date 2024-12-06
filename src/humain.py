e0ac9b6af062773d922a9bbd73a7b04f514435c5
Clownatique <nestor.frtr.vrgnd@gmail.com>
2024-12-06 05:13:37 +0100

    vues humains

--- src/humain.py
+++ src/humain.py
@@ -10,11 +10,10 @@ class Corps(db.Model):
     partie = db.Column(db.String(255), unique=True)
     parrallele = db.relationship(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    numero_schema = db.Column(db.Integer)
 
 
 class Parrallele(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    __tablename__ = 'html_content'
     title = db.Column(String(255))  # Title of the content
     content = db.Column(Text)
+    utilisateur = db.relationship(db._id, nullable=False,) # Chaque parallele a 1 utilisateur => 1 utilisateur peut avoir plusieurs parraleles proposés
